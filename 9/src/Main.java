3a65a7c7c4087af4ec5bd83e4fd7c3a74984648f
Leyan <87895906+IIXIIII@users.noreply.github.com>
2024-06-17 01:26:56 +0800

    finish 9 check palindrome num

--- 9/src/Main.java
+++ 9/src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
   public static void main(String[] args) {
     String tests = "hello world ";
-    System.out.println(tests);
+    System.out.println(isPalindrome(111111));
   }
-  public boolean isPalindrome(int x)  {
-    boolean result = false;
-    int index =0;
-    return true;
+  public static boolean isPalindrome(int x)  {
+    int initial =x;
+    int reversed =0;
+    while (x>0){
+      reversed = reversed*10;
+      reversed = reversed + x%10;
+      x = x/10;
+    }
+    return initial == reversed;
   }
 }
