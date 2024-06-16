c51644e41d2c7876ba76af5300f80a50b75da083
Leyan <87895906+IIXIIII@users.noreply.github.com>
2024-06-17 01:15:45 +0800

    9 TRY

---
+++ 9/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    String tests = "hello world ";
+    System.out.println(tests);
+  }
+  public boolean isPalindrome(int x)  {
+    boolean result = false;
+    int index =0;
+    return true;
+  }
+}
