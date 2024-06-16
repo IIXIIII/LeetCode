c43ec8151b5c317a159992dbeda1a82a95b7cb13
Leyan <87895906+IIXIIII@users.noreply.github.com>
2024-06-17 01:04:25 +0800

    finish 01 two  sum

---
+++ 1/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+  class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      int[] result = new int[2];
+      for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] == target) {
+            result[0] = i;
+            result[1] = j;
+          }
+        }
+      }
+      return result;
+    }
+  }
+}
